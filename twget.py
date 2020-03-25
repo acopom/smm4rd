8a0367706edecb1b2ad024ce9f28a67fcf7494ca
Atsuko Yamaguchi <atsuko.yamaguchi@gmail.com>
2020-03-25 20:50:54 +0900

    Update twget

--- twget.py
+++ twget.py
@@ -11,7 +11,7 @@ auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
 
-for tweet in tweepy.Cursor(api.user_timeline, id="userid").items():
+for tweet in tweepy.Cursor(api.user_timeline, id="userid",tweet_mode='extended').items():
   time.sleep(1)
   if (list(tweet.text)[:2]!=['R', 'T']) & (list(tweet.text)[0]!='@'):
     print(tweet.created_at, end="\t")
